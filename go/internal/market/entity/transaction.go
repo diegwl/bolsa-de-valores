c061efbd71042eb4cc6a877fb7ef66d196b2393f
Lopes Diego (CaP/ETS) <lod7ca@bosch.com>
2023-12-05 08:59:51 -0300

    aula 01

--- go/internal/market/entity/transaction.go
+++ go/internal/market/entity/transaction.go
@@ -1,8 +1,12 @@
 package entity
 
-import "time"
+import (
+	"time"
 
-type Transacion struct {
+	"github.com/google/uuid"
+)
+
+type Transaction struct {
 	ID           string
 	SellingOrder *Order
 	BuyingOrder  *Order
@@ -12,6 +16,39 @@ type Transacion struct {
 	DateTime     time.Time
 }
 
-func NewTransaction(SellingOrder *Order, BuyingOrder *Order, shares int, price float64) *Transacion {
+func NewTransaction(sellingOrder *Order, buyingOrder *Order, shares int, price float64) *Transaction {
 	total := float64(shares) * price
+	return &Transaction{
+		ID:           uuid.New().String(),
+		SellingOrder: sellingOrder,
+		BuyingOrder:  buyingOrder,
+		Shares:       shares,
+		Price:        price,
+		Total:        total,
+		DateTime:     time.Now(),
+	}
+}
+
+func (t *Transaction) CalculateTotal(shares int, price float64) {
+	t.Total = float64(shares) * price
+}
+
+func (t *Transaction) CloseBuyOrder() {
+	if t.BuyingOrder.PendingShares == 0 {
+		t.BuyingOrder.Status = "CLOSED"
+	}
+}
+
+func (t *Transaction) CloseSellOrder() {
+	if t.SellingOrder.PendingShares == 0 {
+		t.SellingOrder.Status = "CLOSED"
+	}
+}
+
+func (t *Transaction) AddBuyOrderPendingShares(shares int) {
+	t.BuyingOrder.PendingShares += shares
+}
+
+func (t *Transaction) AddSellOrderPendingShares(shares int) {
+	t.SellingOrder.PendingShares += shares
 }
